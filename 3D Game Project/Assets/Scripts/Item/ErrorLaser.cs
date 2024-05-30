b178cec078a8b862c70364871b3cf7f1fa297f24
CHICHI <smartb12@naver.com>
2024-05-30 20:16:27 +0900

    Warning Laser 구현

--- 3D Game Project/Assets/Scripts/Item/ErrorLaser.cs
+++ 3D Game Project/Assets/Scripts/Item/ErrorLaser.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ErrorLaser : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    RaycastHit hit;
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] float maxDistance;
+    [SerializeField] float time;
+    public GameObject errorPanel;
 
-    // Update is called once per frame
+    public GameObject curGameObject;
+
     void Update()
     {
-        
+        Debug.DrawRay(transform.position, transform.right * maxDistance, Color.red, 0.3f);
+
+        if(Physics.Raycast(transform.position, transform.right , out hit, maxDistance, layerMask))
+        {
+            if(hit.collider.gameObject != curGameObject)
+            {
+                curGameObject = hit.collider.gameObject;
+                errorPanel.SetActive(true);
+                StartCoroutine(WarningPanel());
+
+            }
+        }
     }
+
+    IEnumerator WarningPanel()
+    {
+        yield return new WaitForSecondsRealtime(time);
+        errorPanel.SetActive(false);
+        curGameObject = null;
+    }
+
+    //public void OnClickDownWarningPanel()
+    //{
+    //    errorPanel.SetActive(false);
+    //}
 }
